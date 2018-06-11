34c0c22041fa831e52e204cf33c9c66234d25330
René Baudisch <rene.baudisch@t-online.de>
2018-06-11 23:52:07 +0200

    added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FlairfireComponent } from './flairfire/flairfire.component';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ToolframeComponent } from './toolframe/toolframe.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { FooterComponent } from './footer/footer.component';
     NavBarComponent,
     FlairfireComponent,
     ToolframeComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     RouterModule,
