072af74898ea025b32b2fe27a6eb27e17acb2d4c
René Baudisch <rene.baudisch@t-online.de>
2018-06-24 22:01:27 +0200

    added zeugs for sylphion testing

--- src/app/toolframe/toolframe.component.ts
+++ src/app/toolframe/toolframe.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-toolframe',
+  selector: 'app-external-integration',
   templateUrl: './toolframe.component.html',
   styleUrls: ['./toolframe.component.css']
 })
