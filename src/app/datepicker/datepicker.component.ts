3288d71cccf4fe5b9a06a70704e722290610e20a
René Baudisch <rene.baudisch@t-online.de>
2018-07-03 00:27:10 +0200

    working on datepicker integration

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -11,13 +11,14 @@ export class DatepickerComponent implements OnInit {
     '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
   monthArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
   yearArray = [];
-  @Input() actualSelection: SelectedDate;
+  @Input() actualSelection: SelectedDate = {
+    day: new Date().getDay() + 1,
+    month: new Date().getMonth() + 1,
+    year: new Date().getFullYear()
+  };
   constructor() { }
 
   ngOnInit() {
-    this.actualSelection.day = new Date().getDay() + 1;
-    this.actualSelection.month = new Date().getMonth() + 1;
-    this.actualSelection.year = new Date().getFullYear();
     for (let i = this.actualSelection.year; i > this.actualSelection.year - 10; i--) {
       this.yearArray.push(i);
     }
